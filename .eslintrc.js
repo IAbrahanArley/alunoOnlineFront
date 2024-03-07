e92c46e3074d0e7f0027b69b31a8e84eaa757eb3
IAbrahanArley <abrahanarley96@gmail.com>
2024-03-07 11:28:17 -0300

    Feat: projeto iniciado e configurado

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/react'
+
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins:[
+    'prettier',
+    'react',
+    'react-hooks'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': 0,
+    'import/prefer-default-export': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+
+  },
+};
